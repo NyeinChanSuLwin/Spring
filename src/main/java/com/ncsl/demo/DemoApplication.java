9568f4768c69bc33c8656cf1066642f45c462752
Nyein Chan <nyeinchansulwin@mm.cybermissions.co.jp>
2019-11-18 09:24:32 +0630

    Deploy

---
+++ src/main/java/com/ncsl/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.ncsl.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+@EnableJpaAuditing
+public class DemoApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
